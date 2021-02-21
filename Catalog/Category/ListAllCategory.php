ca4f9de69e71a03a360f90116daa4187740d39c7
claudio-code <suporte2424@outlook.com>
2021-02-21 20:19:37 -0300

    :sparkles: created resoures  for get all categories and get one category

---
+++ Catalog/Category/ListAllCategory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Catalog\Category;
+
+use App\Models\Category;
+use Catalog\Repository;
+use Illuminate\Database\Eloquent\Collection;
+
+class ListAllCategory
+{
+    private Repository $repository;
+
+    public function __construct(Category $category)
+    {
+        $this->repository = new Repository($category);
+    }
+
+    public function execute(): Collection
+    {
+        return $this->repository->all();
+    }
+}
--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,14 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use Catalog\Category\ListAllCategory;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    public function __construct(
+        private ListAllCategory $listAllCategory
+    ) {
+    }
+
     public function index(): JsonResponse
     {
-        return response()->json();
+        return response()
+            ->json($this->listAllCategory->execute())
+        ;
     }
 
     public function store(Request $request): JsonResponse
@@ -20,7 +28,7 @@ class CategoryController extends Controller
 
     public function show(Category $category): JsonResponse
     {
-        return response()->json();
+        return response()->json($category);
     }
 
     public function update(Request $request, Category $category): JsonResponse
