0f9aa54063772602a04e720326c48c44537a9927
claudio-code <suporte2424@outlook.com>
2021-02-21 00:56:30 -0300

    create new files in database

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -3,6 +3,7 @@
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
 chown -R www-data:www-data .
 composer install
+npm install
 php artisan key:generate
 php artisan migrate
 
