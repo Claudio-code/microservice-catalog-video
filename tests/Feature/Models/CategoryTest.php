b000d158dc87ca59156d9ca0d60d61b8559ba5c0
claudio-code <suporte2424@outlook.com>
2021-03-06 02:11:49 -0300

    :white_check_mark: added new tests for validate uuid v4 and soft delete

--- tests/Feature/Models/CategoryTest.php
+++ tests/Feature/Models/CategoryTest.php
@@ -4,8 +4,6 @@ namespace Tests\Feature\Models;
 
 use App\Models\Category;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CategoryTest extends TestCase
@@ -14,19 +12,16 @@ class CategoryTest extends TestCase
 
     public function testList(): void
     {
-        Category::create([
-            'name' => 'casas12'
-        ]);
-
+        Category::factory()->create();
         $categories = Category::all();
+
         $this->assertCount(1, $categories);
     }
 
     public function testSoftDelete(): void
     {
-        $category = Category::create([
-            'name' => 'casas212'
-        ]);
+        $category = Category::factory()->create();
+        $category->refresh();
         Category::destroy([$category->id]);
         $categories = Category::onlyTrashed()->get()->toArray();
 
@@ -35,13 +30,40 @@ class CategoryTest extends TestCase
 
     public function testCreate(): void
     {
-        $category = Category::create([
-            'name' => 'casas12',
-            'is_active' => true
+        /** @var Category */
+        $category = Category::factory(['name' => 'casas12'])->create();
+        $category->refresh();
+
+        // regex to validadate uuid v4
+        $regex = '/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i';
+
+        $this->assertTrue((bool) preg_match($regex, $category->id));
+        $this->assertEquals('casas12', $category->name);
+        $this->assertTrue($category->is_active);
+    }
+
+    public function testUpdate(): void
+    {
+        /** @var Category */
+        $category = Category::factory()->create();
+        $category->refresh();
+        $category->update([
+            'name' => 'testando',
+            'is_active' => false
         ]);
 
-        $this->assertEquals('casas12', $category->name);
-        $this->assertNull($category->description);
-        $this->assertTrue($category->is_active);
+        $this->assertEquals('testando', $category->name);
+        $this->assertFalse($category->is_active);
+    }
+
+    public function testDelete(): void
+    {
+        /** @var Category */
+        $category = Category::factory()->create();
+        $category->refresh();
+        $category->delete();
+        $categories = Category::all()->toArray();
+
+        $this->assertEquals(0, count($categories));
     }
 }
