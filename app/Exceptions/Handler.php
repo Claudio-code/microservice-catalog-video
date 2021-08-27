a3c6f0dde5c329b2448bed1a1bed9dde9d7c4ce8
claudio-code <suporte2424@outlook.com>
2021-08-27 18:43:05 -0300

    updated error formatted

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,15 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -13,6 +21,10 @@ class Handler extends ExceptionHandler
      * @var mixed[]
      */
     protected $dontReport = [
+        AuthorizationException::class,
+        HttpException::class,
+        ModelNotFoundException::class,
+        ValidationException::class,
     ];
 
     /**
@@ -29,5 +41,29 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
         });
+    }
+
+    public function render($request, Throwable $e): SymfonyResponse | JsonResponse | Response
+    {
+        if ($e instanceof ValidationException) {
+            return \response()->json(
+                [
+                    'error' => ValidationException::class,
+                    'fields' => $this->getErrorsFromValidator($e->validator),
+                ],
+                Response::HTTP_UNPROCESSABLE_ENTITY
+            );
+        }
+
+        return parent::render($request, $e);
+    }
+
+    /** @return array<string> */
+    private function getErrorsFromValidator(Validator $validator): array
+    {
+        return array_map(
+            fn ($messages) => implode(',', $messages),
+            $validator->errors()->messages()
+        );
     }
 }
