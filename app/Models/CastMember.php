d1be656977103f33cbaa081116318f461ba46b2d
claudio-code <suporte2424@outlook.com>
2021-08-27 17:51:34 -0300

    updated entity CastMember to add uuid trait

--- app/Models/CastMember.php
+++ app/Models/CastMember.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Traits\Uuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CastMember extends Model
 {
+    use Uuid;
     use HasFactory;
     use SoftDeletes;
 
